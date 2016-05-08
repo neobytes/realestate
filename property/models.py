cbf50d76d5fdbe31d9d7245aa3f5b526e910ea39
neobytes <dev.kulwantsingh@gmail.com>
2016-05-08 17:51:54 +0530

    added imports in property models

--- property/models.py
+++ property/models.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 class Property(models.Model):
     title = models.CharField(max_length=128, verbose_name=_('Title'))
