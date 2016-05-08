734086be2f79b32d916d7adf436c9386862d91ba
neobytes <dev.kulwantsingh@gmail.com>
2016-05-08 19:44:58 +0530

    adding grappelli admin template

--- realestate/settings.py
+++ realestate/settings.py
@@ -33,6 +33,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # NOTE:third party apps grappelli should be added before adding admin
+    'grappelli',
+
     'property.apps.PropertyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
