3a487a51c68223c202a3d517597d8ba0e66d2536
neobytes <dev.kulwantsingh@gmail.com>
2016-05-10 17:23:35 +0530

    updated basic navigations/links on main page

--- property/views.py
+++ property/views.py
@@ -17,6 +17,57 @@ def add_property(request):
 	print('add property')
 	return render_to_response('add_property.html')
 
+def buying(request):
+	print('buying property')
+	return render_to_response('buying.html')
+
+def selling(request):
+	print('selling property')
+	return render_to_response('selling.html')
+
+
+def mortages(request):
+	print('mortages property')
+	return render_to_response('mortages.html')
+
+def homeforsale(request):
+	print('home for sale property')
+	return render_to_response('homeforsale.html')
+
+def fore(request):
+	print('fore closure')
+	return render_to_response('fore.html')
+
+def lotsandland(request):
+	print('lots and land')
+	return render_to_response('lotsandland.html')
+
+def freehomeplans(request):
+	print('freehomeplans')
+	return render_to_response('freehomeplans.html')
+
+
+def agents(request):
+	print('agents')
+	return render_to_response('agents.html')
+
+def about(request):
+	print('about')
+	return render_to_response('about.html')
+
+def contact(request):
+	print('contact')
+	return render_to_response('contacts.html')
+
+def rental(request):
+	print('rental')
+	return render_to_response('rental.html')
+
+def moving(request):
+	print('moving')
+	return render_to_response('moving.html')
+
+
 @csrf_exempt
 @login_required
 def save_property(request):
--- realestate/urls.py
+++ realestate/urls.py
@@ -20,9 +20,22 @@ urlpatterns = [
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', 'property.views.home', name='home'),
+	url(r'^home', 'property.views.home', name='home'),
 
     # properties
     url(r'property/(\d+)$', 'property.views.property', name='property'),
     url(r'^save_property/$', 'property.views.save_property', name='save_property'),	
 	url(r'^add_property/$', 'property.views.add_property', name='add_property'),
+	url(r'^buying', 'property.views.buying', name='buying'),
+	url(r'^selling', 'property.views.selling', name='selling'),	
+	url(r'^mortages', 'property.views.mortages', name='mortages'),	
+	url(r'^homeforsale', 'property.views.homeforsale', name='homeforsale'),
+	url(r'^fore', 'property.views.fore', name='fore'),	
+	url(r'^lotsandland', 'property.views.lotsandland', name='lotsandland'),	
+	url(r'^freehomeplans', 'property.views.freehomeplans', name='freehomeplans'),	
+	url(r'^agents', 'property.views.agents', name='agents'),
+	url(r'^contact', 'property.views.contact', name='contact'),
+	url(r'^about', 'property.views.about', name='about'),
+	url(r'^moving', 'property.views.moving', name='moving'),
+	url(r'^rental', 'property.views.rental', name='rental'),			
 ]
