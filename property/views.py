9921882b27d24050163e0ddf87e3bd0e830471d6
neobytes <dev.kulwantsingh@gmail.com>
2016-07-13 11:10:02 +0530

    tidy up code

--- property/views.py
+++ property/views.py
@@ -17,44 +17,44 @@ def home(request):
     return render_to_response('index.html', c)
 
 def add_property(request):
-	return render_to_response('add_property.html')
+    return render_to_response('add_property.html')
 
 def buying(request):
-	return render_to_response('buying.html')
+    return render_to_response('buying.html')
 
 def selling(request):
-	return render_to_response('selling.html')
+    return render_to_response('selling.html')
 
 def mortages(request):
-	return render_to_response('mortages.html')
+    return render_to_response('mortages.html')
 
 def homeforsale(request):
-	return render_to_response('homeforsale.html')
+    return render_to_response('homeforsale.html')
 
 def fore(request):
-	return render_to_response('fore.html')
+    return render_to_response('fore.html')
 
 def lotsandland(request):
-	return render_to_response('lotsandland.html')
+    return render_to_response('lotsandland.html')
 
 def freehomeplans(request):
-	return render_to_response('freehomeplans.html')
+    return render_to_response('freehomeplans.html')
 
 
 def agents(request):
-	return render_to_response('agents.html')
+    return render_to_response('agents.html')
 
 def about(request):
-	return render_to_response('about.html')
+    return render_to_response('about.html')
 
 def contact(request):
-	return render_to_response('contacts.html')
+    return render_to_response('contacts.html')
 
 def rental(request):
-	return render_to_response('rental.html')
+    return render_to_response('rental.html')
 
 def moving(request):
-	return render_to_response('moving.html')
+    return render_to_response('moving.html')
 
 
 @csrf_exempt
@@ -83,21 +83,20 @@ def property(request, property_id):
 
 @csrf_exempt
 def main(request):
-	"""
-	authenticate user for username and password
-	"""	
+    """
+    authenticate user for username and password
+    """	
 
-	user = authenticate(username=request.POST.get('user'), password=request.POST.get('passwd'))
-	#request.session['user'] = request.POST.get('user')
-	if user is not None:
-		# the password verified for the user
-		login(request, user)
+    user = authenticate(username=request.POST.get('user'), password=request.POST.get('passwd'))
+    #request.session['user'] = request.POST.get('user')
+    if user is not None:
+	# the password verified for the user
+	login(request, user)
 
-		if user.is_active:
-			return render_to_response('home.html')
-		else:
-			return HttpResponse('The password is valid, but the account has been disabled!')
+	if user.is_active:
+	    return render_to_response('home.html')
 	else:
-		# the authentication system was unable to verify the username and password
-		return HttpResponse('User authentication fail')
-
+	    return HttpResponse('The password is valid, but the account has been disabled!')
+    else:
+	# the authentication system was unable to verify the username and password
+	return HttpResponse('User authentication fail')
